4a7df8cfb8a5ac5684692979aefd7e6f5d503310
Cappy Ishihara <cappy@cappuchino.xyz>
2024-09-08 16:30:44 +0700

    cargo fmt --all

--- src/main.rs
+++ src/main.rs
@@ -51,13 +51,13 @@ fn main() -> Result<()> {
     );
     let args = Boson::parse();
     let exec_path = std::env::current_exe()?;
-    
+
     // get the folder of the executable
     let exec_dir = exec_path.parent().unwrap();
-    
+
     tracing::info!("Executable path: {:?}", exec_path);
     tracing::info!("Executable directory: {:?}", exec_dir);
-    
+
     // Create path for hook
     let hook_path = exec_dir.join("register-hook.js");
 
@@ -68,19 +68,17 @@ fn main() -> Result<()> {
         } => {
             let electron = path_search::env_electron_path();
 
-            let mut args = vec![
-                "--no-sandbox"
-            ];
+            let mut args = vec!["--no-sandbox"];
 
             let gpath = path_search::get_game_path(&game_path);
             // Actually get the game executable path here
             let app_path_str = get_asar_path(&game_path).ok_or_eyre(
                 "Could not find ASAR file in game directory. Make sure you're running this from the game directory.",
             )?;
-            
+
             // todo: path to boson hook
             let load_hook_arg = vec!["--require", hook_path.to_str().unwrap()];
-            
+
             // Add the args before the app path
             args.extend(load_hook_arg.iter());
             args.extend(additional_args.iter().map(|s| s.as_str()));
@@ -89,27 +87,30 @@ fn main() -> Result<()> {
             tracing::info!(?gpath);
 
             tracing::debug!(?args);
-            
+
             // Remove steam overlay from LD_PRELOAD
-            
+
             let ld_preload = std::env::var("LD_PRELOAD").unwrap_or_default();
             // shadow the variable
-            // 
+            //
             // filter out the gameoverlayrenderer
-            let ld_preload = std::env::split_paths(&ld_preload).filter(|x| {
-                x.to_str().map(|x| !x.contains("gameoverlayrenderer")).unwrap_or(true)
-            }).collect::<Vec<_>>();
-            
+            let ld_preload = std::env::split_paths(&ld_preload)
+                .filter(|x| {
+                    x.to_str()
+                        .map(|x| !x.contains("gameoverlayrenderer"))
+                        .unwrap_or(true)
+                })
+                .collect::<Vec<_>>();
+
             let ld_preload = std::env::join_paths(ld_preload).unwrap();
-            
-            
+
             let ld_library_path = std::env::var("LD_LIBRARY_PATH").unwrap_or_default();
             let mut ld_library_path = std::env::split_paths(&ld_library_path).collect::<Vec<_>>();
-            
+
             // add the exec_dir/lib to the LD_LIBRARY_PATH
-            
+
             ld_library_path.push(exec_dir.join("lib"));
-            
+
             let ld_library_path = std::env::join_paths(ld_library_path).unwrap();
 
             let mut cmd = Command::new(electron);
